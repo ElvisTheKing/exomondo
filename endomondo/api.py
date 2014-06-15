9f441df06bee6282f04fa8654ad9e10e33b84037
Sergey Konyukhovsky <sk@onthe.net.ru>
2014-06-15 18:24:32 +0400

    getting auth token from mobile api

---
+++ endomondo/api.py
@@ -0,0 +1,47 @@
+import requests
+import logging
+import uuid
+import re
+
+logging.basicConfig(level=logging.DEBUG)
+
+class Api(object):
+    def __init__(self, 
+                email = None, 
+                password = None, 
+                user_agent = None, 
+                auth_token = None):
+        
+        if email and password:
+            self.email = email
+            self.password = password
+        
+        self.session = requests.Session()
+        if user_agent:
+            self.session.headers.update({'User-Agent': user_agent})
+
+        if (not auth_token):
+            if not (email and password):
+                raise Exception('you should specify email and password or auth token')
+            self.auth_token = self.request_auth_token()
+        else:
+            self.auth_token = auth_token
+
+    def request_auth_token(self):
+        url = 'https://api.mobile.endomondo.com/mobile/auth'
+        params = { 
+            'action': 'pair',
+            'email':    self.email,
+            'password': self.password,
+            'country': 'GB',
+            'deviceId': str(uuid.uuid4())
+        }
+
+        r = self.session.get(url,params = params)
+        if (r.text.strip()[:2] != "OK"):
+            raise Exception("auth token request failed", r.text)
+
+        m = re.search(r'authToken=(.+)',r.text)
+        logging.debug('Got auth token from endomondo: %s' % m.group(1))
+        return m.group(1)
+
