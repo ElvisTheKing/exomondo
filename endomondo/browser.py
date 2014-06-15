58c974937115047babcc060537abab5079838ac9
Sergey Konyukhovsky <sk@onthe.net.ru>
2014-06-15 22:08:27 +0400

    logging in to endomondo.com and downloading track

---
+++ endomondo/browser.py
@@ -0,0 +1,50 @@
+import splinter
+import requests
+import logging
+import re
+
+logging.basicConfig(level=logging.DEBUG)
+
+class Browser(object):
+    def __init__(self,email,password,user_agent = None,cookies = None):
+
+        self.browser = splinter.Browser('firefox',user_agent=user_agent)
+
+        self.session = requests.Session()
+        self.session.headers.update({'User-Agent': user_agent})
+
+        if (not cookies):
+            self.cookies = self.login(email,password)
+        else:
+            self.cookies = cookies
+
+    def login(self,email,password):
+        b = self.browser
+
+        b.visit('http://www.endomondo.com')
+        b.find_link_by_href('login').first.click()
+
+        b.fill('email',email)
+        b.fill('password',password)
+        b.find_by_css('div.signInButton').first.click()
+        cookies = dict(map(lambda cookie: (cookie['name'],cookie['value']), b.cookies.all()))
+
+        return cookies
+
+    def download_track(self,workout_id):
+        url = 'http://www.endomondo.com/workouts/%d' % workout_id
+
+        b = self.browser
+        b.visit(url)
+        b.find_by_css('span.more').first.click()
+        b.find_by_css('a.export').first.click()
+        b.screenshot('/Users/zz/Dropbox/Workspace/python/endomondo_track_downloader/bin/scr/')
+        m = re.search('<a href="\.\./(.+?exportGpxLink.+?)">',b.html)
+        
+        url = 'http://www.endomondo.com/'+m.group(1)
+        r = self.session.get(url, cookies = self.cookies)
+        return r.text
+
+
+    def get_cookies(self):
+        return self.session.cookies
